1bb3c28b6cd7e7f8686503345158f4bf2b40be6c
hikari <runrunrun4649@icloud.comm>
2024-03-10 18:02:42 +0900

    conflict a

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace "api" do
     resources :users, only: %i[create update]
     # アクションメソッドlogin
+    # conflict文章A 
     post 'login', to: 'authentication#login'
   end
 end
